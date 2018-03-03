c7400ca97d5347e4c750a1726e59aa4bd932c879
Aleksandr Drozd <alex@kfc.r.gsic.titech.ac.jp>
2018-03-04 08:05:16 +0900

    run sripts for mpi cuda check on kfc

---
+++ mpi_cuda/run_kfc.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=2
+#SBATCH --job-name=mnist
+#SBATCH -t 0-0:20 # time (D-HH:MM) 
+
+
+#source ./modules.sh
+
+source /home/users/alex/apps.sh
+
+mpirun \
+  -output-proctable \
+  -mca pml ob1 \
+  -np 4 \
+  -npernode 2 \
+  -x PATH \
+  -x LD_LIBRARY_PATH \
+  -x LIBRARY_PATH \
+   a.out
