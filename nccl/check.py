70ecb096d8ac4050df74c55e890ed5091a7ff741
Aleksandr Drozd <alex@kfc.r.gsic.titech.ac.jp>
2018-03-05 15:47:14 +0900

    catch exception

--- nccl/check.py
+++ nccl/check.py
@@ -1,9 +1,17 @@
 import chainermn
+from chainermn import nccl
+from mpi4py import MPI
 
 
 def main():
     comm = chainermn.create_communicator("hierarchical")
-    print(f"hi from {comm.rank}")
+    device = comm.intra_rank
+    host = MPI.Get_processor_name()
+    print(f"r {comm.rank} of {comm.size} [intra {comm.intra_rank} of {comm.intra_size}] on {host}")
+    try:
+        comm._init_comms()
+    except:
+        print(f"failed rank {comm.rank} intra {comm.intra_rank} of {comm.intra_size} on {host}")
 
 
 if __name__ == "__main__":
--- nccl/run_kfc.sh
+++ nccl/run_kfc.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #SBATCH --nodes=32
 #SBATCH --ntasks-per-node=8
-#SBATCH --job-name=imagenet
+#SBATCH --job-name=chnrml_chk
 #SBATCH -t 0-0:20 # time (D-HH:MM) 
 
-#  -mca pml ob1 \
 
 mpirun \
   -output-proctable \
+  -mca pml ob1 \
   -np 256 \
   -npernode 8 \
   -x PATH \
