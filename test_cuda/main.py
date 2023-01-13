2dd994013b90d1479c78f1b50117196972e3c7dd
Aleksandr Drozd <alexander.drozd@gmail.com>
2023-01-13 13:17:40 +0900

    allocating tensors

--- test_cuda/main.py
+++ test_cuda/main.py
@@ -1,6 +1,19 @@
 import torch
 
-for id_device in range(torch.cuda.device_count()):
-    print(f"checking device {id_device}:")
+cnt_devices = torch.cuda.device_count()
+print("cuda initialized:", torch.cuda.is_initialized())
+print("cnt cnt_devices:", cnt_devices)
+for id_device in range(cnt_devices):
+    device_name = torch.cuda.get_device_name(id_device)
+    print(f"checking device {id_device} {device_name}:")
+    torch.cuda.set_device(id_device)
     properties = torch.cuda.get_device_properties(id_device)
     print(properties)
+    mem_size = properties.total_memory
+    print("creating tensor on device")
+    chunks = []
+    cnt_chunks = 10
+    for id_chunk in range(cnt_chunks):
+        chunks.append(torch.rand(int(0.7 * mem_size / 4 / cnt_chunks), dtype=torch.float32, device=id_device))
+
+# torch.cuda.can_device_access_peer(device, peer_device)[source]
