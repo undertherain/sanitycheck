a2376f1ee74692c9fe17c832ebb83b9b49bfd462
Sasha <alexander.drozd@gmail.com>
2023-02-06 11:18:21 +0900

    init mpi process group

--- stdp/main.py
+++ stdp/main.py
@@ -1,3 +1,5 @@
+import datetime
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -18,8 +20,35 @@ class MyModule(nn.Module):
 
 
 # torch.cuda.set_device(device_id)
+print("Hi form pytorch", torch.__version__)
 my_module = MyModule()
-sharded_module = FSDP(my_module)
+
+backend = "mpi"  # TODO: use class for more controll
+
+process_group = torch.distributed.init_process_group(
+    backend,
+    init_method=None,
+    timeout=datetime.timedelta(seconds=1800),
+    world_size=-1,
+    rank=-1,
+    store=None,
+    group_name='',
+    pg_options=None)
+
+sharded_module = FSDP(
+    my_module,
+    process_group=process_group,
+    sharding_strategy=None,
+    cpu_offload=None,
+    auto_wrap_policy=None,
+    backward_prefetch=None,
+    mixed_precision=None,
+    ignored_modules=None,
+    param_init_fn=None,
+    device_id=None,
+    sync_module_states=False,
+    forward_prefetch=False,
+    limit_all_gathers=False)
 # sharded_module = my_module
 optim = torch.optim.Adam(sharded_module.parameters(), lr=0.0001)
 x = torch.rand(256)
