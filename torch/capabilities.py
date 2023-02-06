8fb167d2c7996d9f25d7b855ca26a5c25f6a474f
Sasha <alexander.drozd@gmail.com>
2023-02-06 13:47:22 +0900

    cuda avial

--- torch/capabilities.py
+++ torch/capabilities.py
@@ -1,6 +1,7 @@
 import torch
 
 print("torch version", torch.__version__)
+print("cuda avail:\t", torch.cuda.is_available())
 print("distr avail:\t", torch.distributed.is_available())
 print("mpi avail:\t", torch.distributed.is_mpi_available())
 print("nccl avail:\t", torch.distributed.is_nccl_available())
