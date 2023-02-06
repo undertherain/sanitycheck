c774e3e0c8cbbcb66e4e66d7768b96027e4ae891
Sasha <alexander.drozd@gmail.com>
2023-02-06 13:50:03 +0900

    align print

--- torch/capabilities.py
+++ torch/capabilities.py
@@ -1,7 +1,7 @@
 import torch
 
 print("torch version", torch.__version__)
-print("cuda avail:\t", torch.cuda.is_available())
-print("distr avail:\t", torch.distributed.is_available())
-print("mpi avail:\t", torch.distributed.is_mpi_available())
-print("nccl avail:\t", torch.distributed.is_nccl_available())
+print("cuda avail: ", torch.cuda.is_available())
+print("distr avail:", torch.distributed.is_available())
+print("mpi avail:  ", torch.distributed.is_mpi_available())
+print("nccl avail: ", torch.distributed.is_nccl_available())
