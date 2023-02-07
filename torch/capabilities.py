2ef3dd31226326344ad6c7aeabdc80ec298148d9
Sasha <alexander.drozd@gmail.com>
2023-02-07 14:14:27 +0900

    python details

--- torch/capabilities.py
+++ torch/capabilities.py
@@ -1,6 +1,13 @@
+import sys
+
 import torch
 
-print("torch version", torch.__version__)
+print("python version:", sys.version_info)
+print("running from  :", sys.executable)
+print()
+print("torch version : ", torch.__version__)
+
+print()
 print("cuda avail: ", torch.cuda.is_available())
 print("distr avail:", torch.distributed.is_available())
 print("mpi avail:  ", torch.distributed.is_mpi_available())
