559ed2aef69f28c3f89d076ad4d24704e6ff4663
Aleksandr Drozd <alexander.drozd@gmail.com>
2024-09-02 15:31:08 +0900

    check mem

--- wl/main.cpp
+++ wl/main.cpp
@@ -1,12 +1,28 @@
 #include <mpi.h>
 #include <iostream>
 #include <chrono>
+#include <thread>
+#include <algorithm>
 
 int main(const int argc, const char * argv[])
 {
     MPI_Init(NULL, NULL);
-    std::cout<<"hi";
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    std::cout<<"hi again";
+    // std::cout<<"hi\n";
+    const size_t size = 10000000;
+    char* buf = new char[size];
+    std::fill(buf, buf + size, 0);
+
+    for (size_t i=0; i<100500; i++)
+    {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        for (size_t j=0; j<size; j++)
+        {
+            if (buf[j]!=0)
+            std::cerr<<j;
+        }
+
+    }
+    // std::cout<<"hi again\n";
+    delete[] buf;
     return 0;
 }
