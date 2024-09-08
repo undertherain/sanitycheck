b77e4c712ba70fe07f93b2bdf44b3fe4f600ac3d
Aleksandr Drozd <alexander.drozd@gmail.com>
2024-09-08 14:14:06 +0900

    report hostname

--- torch/test_ddp/test.py
+++ torch/test_ddp/test.py
@@ -1,7 +1,8 @@
+from socket import gethostname
+
 import torch
 
 torch.distributed.init_process_group(backend="gloo")
-                                     
                                      #timeout=datetime.timedelta(seconds=1800),
                                      #world_size=- 1,
                                      #rank=- 1,
@@ -11,7 +12,7 @@ torch.distributed.init_process_group(backend="gloo")
 size = torch.distributed.get_world_size()
 # print("size:", size, type(size))
 rank = torch.distributed.get_rank()
-print(f"rank: {rank} out of {size}")
+print(f"rank: {rank} out of {size}, running on {gethostname()}")
 data = torch.tensor(1).cuda()
 torch.distributed.all_reduce(data)
 #print(data)
